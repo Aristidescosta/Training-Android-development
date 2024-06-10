99e99f1bbf2c22f3f06f460447e5f36a8df38a43
Aristidescosta <aristidiscosta200@gmail.com>
2024-06-10 11:44:12 +0100

    refactor: criando cartão de visita

--- app/src/main/java/com/example/training/MainActivity.kt
+++ app/src/main/java/com/example/training/MainActivity.kt
@@ -3,17 +3,35 @@ package com.example.training
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.training.ui.theme.TrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +42,10 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
+                    contentColor = Color(0xFF3ddc84)
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -34,12 +53,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = "Olá, meu nome é $name!",
-            modifier = modifier.padding(24.dp)
-        )
+fun Card(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.iam),
+                contentDescription = null
+            )
+            Text(
+                text = "Aristides da Costa",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Desenvolvedor de Android & Web",
+                fontSize = 16.sp
+            )
+        }
+        
+        Column() {
+            IconsDetails(
+                icon = Icons.Default.Phone,
+                iconTitle = "+244 938-260-570",
+                iconDescrible = "Ícone de telefone"
+            )
+            IconsDetails(
+                icon = Icons.Default.Share,
+                iconTitle = "Desenvolvedor Android & Mobile",
+                iconDescrible = "Ícone de partilha"
+            )
+            IconsDetails(
+                icon = Icons.Default.Email,
+                iconTitle = "aristidiscosta200@gmail.com",
+                iconDescrible = "Ícone de email"
+            )
+        }
+    }
+}
+
+@Composable
+fun IconsDetails(
+    icon: ImageVector,
+    iconTitle: String,
+    iconDescrible: String?,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Icon(icon, contentDescription = iconDescrible)
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = iconTitle)
     }
 }
 
@@ -47,6 +120,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TrainingTheme {
-        Greeting("Aristides da Costa")
+        Card(modifier = Modifier.fillMaxSize())
     }
 }
