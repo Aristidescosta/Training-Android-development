b75cee391fc1f0cc53dfd5785049d74a694c026e
Aristidescosta <aristidiscosta200@gmail.com>
2024-06-10 10:39:17 +0100

    refactor: criando artigo do jetpack compose

--- app/src/main/java/com/example/training/MainActivity.kt
+++ app/src/main/java/com/example/training/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.training
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.training.ui.theme.TrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArticleScreen()
                 }
             }
         }
@@ -34,19 +43,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
+fun ArticleScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        BannerImage(modifier = Modifier.fillMaxWidth())
         Text(
-            text = "Olá, meu nome é $name!",
-            modifier = modifier.padding(24.dp)
+            text = stringResource(R.string.jetpack_compose_tutorial),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.subtitle),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(horizontal = 16.dp
+            )
+        )
+        Text(
+            text = stringResource(R.string.description),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
         )
     }
+}
+
+@Composable
+fun BannerImage(modifier: Modifier = Modifier) {
+    val imageId = painterResource(id = R.drawable.bg_compose_background)
+    Image(
+        painter = imageId,
+        contentDescription = stringResource(R.string.banner_do_jetpack_compose),
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TrainingTheme {
-        Greeting("Aristides da Costa")
+        ArticleScreen()
     }
 }
