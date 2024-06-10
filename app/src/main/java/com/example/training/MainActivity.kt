93987cae32060d67b22674fab62f172fb2614fca
Aristidescosta <aristidiscosta200@gmail.com>
2024-06-10 17:08:27 +0100

    refactor: esboço do card de animes

--- app/src/main/java/com/example/training/MainActivity.kt
+++ app/src/main/java/com/example/training/MainActivity.kt
@@ -3,17 +3,40 @@ package com.example.training
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.training.ui.theme.TrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +49,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CharacterCard()
                 }
             }
         }
@@ -34,12 +57,108 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = "Olá, meu nome é $name!",
-            modifier = modifier.padding(24.dp)
+fun CharacterCard(modifier: Modifier = Modifier) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .width(240.dp),
+        colors = CardDefaults.cardColors(
+            contentColor = Color.White
         )
+    ){
+        Box(modifier = Modifier.background(color = Color.DarkGray)){
+            Surface(
+                color = Color.Green,
+                modifier = Modifier
+                    .height(24.dp)
+                    .width(36.dp)
+                    .clip(shape = Shapes().extraSmall),
+            ) {
+                Text(text = "14", textAlign = TextAlign.Center)
+            }
+            Row(modifier = Modifier.padding(16.dp)) {
+                Box(modifier = Modifier.align(Alignment.Bottom)) {
+                    Text(text = "Modo Kaioken")
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.character1),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(194.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+        Column(){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Goku", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "422")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Box(
+                        modifier = Modifier
+                            .background(Color.Gray)
+                            .padding(4.dp)
+                            .width(32.dp)
+                            .clip(RoundedCornerShape(32.dp))
+                    ){
+                        Text(text = "HP")
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column() {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Text(text = "Poder")
+                Text(text = "65")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            LinearProgressIndicator(
+                progress = (65.toFloat()/100),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(8.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                color = Color.Red
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Column() {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Text(text = "Poder")
+                Text(text = "55")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            LinearProgressIndicator(
+                progress = (55.toFloat() / 100),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(8.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                color = Color.Blue
+            )
+        }
     }
 }
 
@@ -47,6 +166,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TrainingTheme {
-        Greeting("Aristides da Costa")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            CharacterCard()
+        }
     }
 }
